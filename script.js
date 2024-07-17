e2ecb153436a0cb20632bbfc334c36b85538758c
niti2004 <151439949+niti2004@users.noreply.github.com>
2024-07-17 20:12:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const images = document.querySelectorAll('.skill-image');
+    const lightbox = document.getElementById('lightbox');
+    const lightboxImg = document.getElementById('lightbox-img');
+    const close
